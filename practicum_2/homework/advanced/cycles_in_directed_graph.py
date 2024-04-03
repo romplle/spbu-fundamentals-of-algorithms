fa6ec2d14ede120c70b9ea3fda2a42c6e63e9ff4
Kirill Serikov <glugol100@gmail.com>
2024-04-03 21:25:25 +0300

    [KSerikov] Advanced homework practicum_2

--- practicum_2/homework/advanced/cycles_in_directed_graph.py
+++ practicum_2/homework/advanced/cycles_in_directed_graph.py
@@ -3,21 +3,38 @@ import networkx as nx
 
 TEST_GRAPH_FILES = [
     "graph_1_wo_cycles.edgelist",
-    "graph_2_w_cycles.edgelist",
+    "graph_2_wo_cycles.edgelist",
+    "graph_3_w_cycles.edgelist",
 ]
 
 
 def has_cycles(g: nx.DiGraph):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    visited = set()
+    stack = set()
 
-    pass
+    def dfs(node):
+        if node in stack:
+            return True
+        if node in visited:
+            return False
+
+        visited.add(node)
+        stack.add(node)
+
+        for neighbor in g.neighbors(node):
+            if dfs(neighbor):
+                return True
+
+        stack.remove(node)
+        return False
+
+    for node in g.nodes:
+        if dfs(node):
+            return True
+    return False
 
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
-        # Load the graph
-        G = nx.read_edgelist("practicum_2/homework/graph_2.edgelist", create_using=nx.DiGraph)
-        # Output whether it has cycles
+        G = nx.read_edgelist(f"practicum_2/homework/advanced/{filename}", create_using=nx.DiGraph)
         print(f"Graph {filename} has cycles: {has_cycles(G)}")
