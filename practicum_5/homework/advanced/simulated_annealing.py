eeeb9cdc70b6ad44a6268cea9d13ef58a713c150
Kirill Serikov <glugol100@gmail.com>
2024-05-04 02:01:39 +0300

    [KSerikov] Advanced homework practicum_5

---
+++ practicum_5/homework/advanced/simulated_annealing.py
@@ -0,0 +1,75 @@
+import numpy as np
+from numpy.typing import NDArray
+import networkx as nx
+
+from src.plotting import plot_graph, plot_loss_history
+
+
+NDArrayInt = NDArray[np.int_]
+
+
+def number_of_conflicts(G, colors):
+    set_colors(G, colors)
+    n = 0
+    for n_in, n_out in G.edges:
+        if G.nodes[n_in]["color"] == G.nodes[n_out]["color"]:
+            n += 1
+    return n
+
+
+def set_colors(G, colors):
+    for n, color in zip(G.nodes, colors):
+        G.nodes[n]["color"] = color
+
+
+def tweak(colors, n_max_colors):
+    new_colors = colors.copy()
+    n_nodes = len(new_colors)
+    random_i = np.random.randint(low=0, high=n_nodes)
+    random_color = np.random.randint(low=0, high=n_max_colors)
+    new_colors[random_i] = random_color
+    return new_colors
+
+
+def solve_via_simulated_annealing(
+    G: nx.Graph, n_max_colors: int, initial_colors: NDArrayInt, n_iters: int
+):
+    loss_history = np.zeros((n_iters,), dtype=np.int_)
+    current_colors = initial_colors
+    current_loss = number_of_conflicts(G, current_colors)
+    best_loss = current_loss
+    temperature = 1000.0
+
+    for i in range(1, n_iters + 1):
+        temperature = temperature * (1 / (np.log(n_iters + 330) * 0.1896))
+        print(temperature)
+        new_colors = tweak(current_colors, n_max_colors)
+        new_loss = number_of_conflicts(G, new_colors)
+
+        if new_loss < current_loss or (temperature > 0 and np.random.rand() < np.exp((current_loss - new_loss) / temperature)):
+            current_colors = new_colors
+            current_loss = new_loss
+
+        if new_loss < best_loss:
+            best_colors = new_colors
+            best_loss = new_loss
+
+        loss_history[i - 1] = current_loss
+
+    return loss_history
+
+
+if __name__ == "__main__":
+    seed = 42
+    np.random.seed(seed)
+    G = nx.erdos_renyi_graph(n=100, p=0.05, seed=seed)
+    plot_graph(G)
+
+    n_max_iters = 500
+    n_max_colors = 3
+    initial_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
+
+    loss_history = solve_via_simulated_annealing(
+        G, n_max_colors, initial_colors, n_max_iters
+    )
+    plot_loss_history(loss_history)
