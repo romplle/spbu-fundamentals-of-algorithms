ae5fdc958008d877fcb3bd0c84fae26c38a0e45d
Kirill Serikov <glugol100@gmail.com>
2024-05-10 23:40:25 +0300

    [KSerikov] Basic homework practicum_7

--- practicum_7/homework/basic/inverse_power_method.py
+++ practicum_7/homework/basic/inverse_power_method.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 from src.common import NDArrayFloat
 
 
-def inverse_power_method(A: NDArrayFloat, n_iters: int) -> NDArrayFloat:
-
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    pass
-
+def inverse_power_method(A: NDArrayFloat, n_iters: int) -> float:
+    n = A.shape[0]
+    eigenvector = np.random.rand(n)
+    A_inverse = np.linalg.inv(A)
+    for i in range(n_iters):
+        eigenvector = np.dot(A_inverse, eigenvector)
+        eigenvalue = max(abs(eigenvector))
+        eigenvector = eigenvector / eigenvalue
+    return 1 / (eigenvalue + 1e-10)
 
 if __name__ == "__main__":
     A = np.array(
@@ -22,4 +23,7 @@ if __name__ == "__main__":
             [2.0, -1.0, 1.0, 1.0],
         ]
     )
-    eigvals = inverse_power_method(A, n_iters=10)
+
+    n_iters = 100
+    smallest_eigenvalue = inverse_power_method(A, n_iters)
+    print("Smallest eigenvalue:", smallest_eigenvalue)
